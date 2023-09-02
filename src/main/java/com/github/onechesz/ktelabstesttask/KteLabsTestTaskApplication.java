fb59d20b90e81bb5c6424de4942356e92dc64a36
Ivan Minyaev <i.onechesz@gmail.com>
2023-09-02 13:13:12 +0300

    Initalize

---
+++ src/main/java/com/github/onechesz/ktelabstesttask/KteLabsTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.github.onechesz.ktelabstesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KteLabsTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KteLabsTestTaskApplication.class, args);
+	}
+
+}
